25fa09b2bbd175c6cb27b1c0fd7949f73af38f0d
BotAnjayy4 <indracookk9@gmail.com>
2022-03-01 22:54:37 +0700

    tesss

---
+++ ryudata/welkam.js
@@ -0,0 +1,19 @@
+//EDIT SESUKA KALIAN 😉
+
+// UCAPAN WELCOME
+exports.welkam = () => {
+	return `Baru Join? Hadeh Intro Klo Nganggur
+
+📛 Nama : 
+🔞 Umur :
+🏙️ Askot :
+
+Klo Udh Join Gosah Out Lagi Su
+`
+}
+
+// UCAPAN GOODBYE
+exports.goodbye = () => {
+	return `Ga Niat Join Gosah Join Asu
+`
+}
